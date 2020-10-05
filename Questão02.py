faf5378a51d22292fbd825f564311b2c0721e4fd
Larissa Moreira <64422214+Larissamoreiram@users.noreply.github.com>
2020-10-05 13:30:17 -0300

    Update Questão02.py

--- Questão02.py
+++ Questão02.py
@@ -1,11 +1,22 @@
-'''Elabore um programa para solicitar o nome de uma equipe de futebol, a
-quantidade de derrotas, empates e vitórias obtidas por ela no campeonato.
-Calcular e informar: a quantidade de pontos ganhos e a quantidade de pontos
-perdidos.'''
+'''
+Elabore um programa para solicitar o nome de uma equipe de futebol,
+a quantidade de derrotas, empates e vitórias obtidas por ela no campeonato.
+No futebol, cada vitória vale três pontos e cada empate vale um ponto.
+Calcular e informar: a quantidade de pontos ganhos,
+a quantidade de pontos perdidos e o percentual de aproveitamento da equipe
+'''
 
 time = str(input('Digite o nome do time: '))
 vitorias = int(input('Digite o número de vitórias: '))
 derrotas = int(input('Digite o número de derrotas: '))
 empates = int(input('Digite o número de empates: '))
-print('Pontos ganhos: ', (vitorias * 3 + empates))
-print('Pontos perdidos: ', (derrotas * 3 % vitorias))
+
+pts_ganhos = vitorias * 3 + empates
+pts_perdidos = derrotas * 3 + empates * 2
+total_pts = pts_ganhos + pts_perdidos
+aproveitamento = (pts_ganhos/total_pts) * 100
+
+print(f'{time}:')
+print(f'Pontos ganhos: {pts_ganhos}')
+print(f'Pontos perdidos: {pts_perdidos}')
+print(f'Aproveitamento de {aproveitamento:.2f}%')
